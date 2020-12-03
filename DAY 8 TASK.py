077afa5db6e3cf7e47bb7cbef67131c704df3f10
ritikasharma001 <75036287+ritikasharma001@users.noreply.github.com>
2020-12-03 21:41:48 +0530

    Add files via upload

---
+++ DAY 8 TASK.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # DAY 8 TASK
+
+# In[1]:
+
+
+All Types of Errors
+AssertionError => Raised when an assert statement fails.
+
+AttributeError => Raised when attribute assignment or reference fails.
+
+EOFError => Raised when the input() function hits end-of-file condition.
+
+FloatingPointError => Raised when a floating point operation fails.
+
+GeneratorExit => Raise when a generator's close() method is called.
+
+ImportError => Raised when the imported module is not found.
+
+IndexError => Raised when the index of a sequence is out of range.
+
+KeyError => Raised when a key is not found in a dictionary.
+
+KeyboardInterrupt => Raised when the user hits the interrupt key (Ctrl+C or Delete).
+
+MemoryError => Raised when an operation runs out of memory.
+
+NameError => Raised when a variable is not found in local or global scope.
+
+NotImplementedError => Raised by abstract methods.
+
+OSError => Raised when system operation causes system related error.
+
+OverflowError => Raised when the result of an arithmetic operation is too large to be represented.
+
+
+# In[ ]:
+
+
+OverflowError => Raised when the result of an arithmetic operation is too large to be represented.
+
+ReferenceError => Raised when a weak reference proxy is used to access a garbage collected referent.
+
+RuntimeError => Raised when an error does not fall under any other category.
+
+StopIteration => Raised by next() function to indicate that there is no further item to be returned by iterator.
+
+SyntaxError => Raised by parser when syntax error is encountered.
+
+IndentationError => Raised when there is incorrect indentation.
+
+TabError => Raised when indentation consists of inconsistent tabs and spaces.
+
+SystemError => Raised when interpreter detects internal error.
+
+SystemExit => Raised by sys.exit() function.
+
+TypeError => Raised when a function or operation is applied to an object of incorrect type.
+
+UnboundLocalError => Raised when a reference is made to a local variable in a function or method, but no value has been bound to that variable.
+
+UnicodeError => Raised when a Unicode-related encoding or decoding error occurs.
+
+UnicodeEncodeError => Raised when a Unicode-related error occurs during encoding.
+
+UnicodeDecodeError => Raised when a Unicode-related error occurs during decoding.
+
+UnicodeTranslateError => Raised when a Unicode-related error occurs during translating.
+
+ValueError => Raised when a function gets an argument of correct type but improper value.
+ZeroDivisionError => Raised when the second operand of division or modulo operation is zero.
+
+
+# In[2]:
+
+
+# Task 2
+print("Simple Calculator")
+print("Select the operation to be performed:")
+operation = input("Enter operator to use:")
+opr_1 = int(input("Enter first number: "))
+opr_2 = int(input("Enter second number: "))
+try:
+    if operation == '+':
+       print(opr_1,"+",opr_2,"=", opr_1+opr_2)
+    elif operation == '-':
+       print(opr_1,"-",opr_2,"=", opr_1-opr_2)
+    elif operation == '*':
+       print(opr_1,"*",opr_2,"=", opr_1*opr_2)
+    elif operation == '/':
+       print(opr_1,"/",opr_2,"=", opr_1/opr_2)
+    else:
+       print("Please enter a valid operation")
+except Exception as e:
+            print("Error : {}".format(str(e)))
+
+
+# In[3]:
+
+
+# Task 3
+try:
+  print(x)
+except NameError:
+  print("Variable x is not defined")
+except:
+  print("Other Error")
+
+
+# In[4]:
+
+
+# Task 5
+x = 10
+try:
+    x = int(input())
+    print(x)
+except:
+    print("Error no Number was entered")
+
+
+# In[ ]:
+
+
+
+
